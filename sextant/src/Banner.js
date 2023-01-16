85335c385d1c7ee4bcb08d560963caa44dffbbca
cytxiwai <98063373+cytxiwai@users.noreply.github.com>
2023-01-15 21:01:35 -0800

    changed banner, exhibit height

--- sextant/src/Banner.js
+++ sextant/src/Banner.js
@@ -1,11 +1,16 @@
-const Banner = () => {
-    return (
-        <div className="m-2 w-80 bg-warning text-white rounded">
-            <img src="./image/sextant-logo.png" alt="" className="w-25 p-3 mh-25"/>
-            <h1>Sextant</h1>
-        </div>
-    )
+import React, {Component}from 'react';
+import './Banner.css';
 
+class Banner extends Component {
+    render() {
+        return (
+            <div className="Banner m-2 w-80 bg-warning text-white rounded">
+                <img src="./image/sextant-logo.png" alt="" className="BannerLogo w-25 p-5 pt-2"/>
+                <h1>{this.props.bannerText}</h1>
+            </div>
+        );
+
+    }
 }
 
 export default Banner;
--- sextant/src/Exhibit.js
+++ sextant/src/Exhibit.js
@@ -1,10 +1,15 @@
-const Exhibit = (props) => {
-    return(
-        <div className="m-2 w-25 bg-secondary d-inline-block float-start rounded">
-        <h2>heading</h2>
-        <h3>props {props.children}</h3>
-        </div>
-        )
+import React, {Component} from 'react';
+import './Exhibit.css';
+
+class Exhibit extends Component {
+    render(){
+        return(
+            <div className="Exhibit m-2 w-25 bg-secondary d-inline-block float-start rounded">
+            <h2 className="ExhibitHeading">{this.props.name}</h2>
+            <h3> {this.props.children}</h3>
+            </div>
+            );
+    }
 
 }
 export default Exhibit;
